a4dfa113203f54022c934d18e6d29ff0fcd8b8d4
shanieMoonlight <shanebrannick@yahoo.com>
2021-02-09 11:03:38 +0000

    Add project files.

---
+++ ClickOnceHelper/HelperClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ClickOnceHelper
+{
+   public class HelperClass
+   {
+      public static int DoSomething(int i) => i * 10;
+
+   }
+}
---
+++ ClickOnceWpf/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ClickOnceHelper;
+using System.Windows;
+
+namespace ClickOnceWpf
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+         HelperClass.DoSomething(7);
+      }
+   }
+}
